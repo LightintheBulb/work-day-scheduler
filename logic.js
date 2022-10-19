c92876c3f5e481e6cc49df925aed530943857e08
Lightinthebulb <>
2022-10-18 20:24:29 -0400

    Added the following: Line 5:8 | GrabbedNearby Values Line 11 | Added LocalStorage Code Line 18:25| Added code to Display/Hide Notification message Line 30:31| Added moment.js code to grab current hour Ling 34:53| Added code to apply logic to schedule blocks to apply color based on blockhour compared to currentHour line 67:74| Added code to store value entered in timeblocks into local storage Line 79:82| Added code to display currentDay and currentTime in the header

--- logic.js
+++ logic.js
@@ -2,25 +2,59 @@ $(document).ready(function () {
     // listen for save button clicks
     $('.saveBtn').on('click', function () {
         // get nearby values
+        var value = $(this).siblings('.description').val();
+        var time = $(this).parent().attr('id')
+        console.log("this", time)
+        console.log("value", value)
 
         // save in localStorage
-
+        localStorage.setItem(time, value)
+        
         // Show notification that item was saved to localStorage by adding class 'show'
 
         // Timeout to remove 'show' class after 5 seconds
+        
         setTimeout(function () {
-            $('.notification').removeClass('show');
-        }, 5000);
+            $('.notification').removeClass('d-none');
+        }, 250);
+        
+        console.log("123")
+
+        setTimeout(function () {
+            $('.notification').addClass('d-none');
+        }, 3250);
     });
 
     function hourUpdater() {
         // get current number of hours (preferably with moment.js)
+        var currentHour = moment().hours();
+        console.log("currentHour", currentHour)
 
         // loop over time blocks
+        var eachFunction = function() {
+            
+            var blockHour = parseInt($(this).attr('data-hour'))
+            console.log(blockHour)
+
+            // check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass('past');
+            } else if (blockHour === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        }
+
+        var timeBlock = $('.time-block').each(eachFunction)
+
         // loop over time blocks ---> https://api.jquery.com/each/
         // inside this loop, // check if we've moved past this time. If we have, make the row grey. If it's future, make it green. if it's past, make it red. Using the past, present, and future classes in css file
 
-        // check if we've moved past this time
+        
     }
 
     hourUpdater();
@@ -30,8 +64,20 @@ $(document).ready(function () {
 
     // load any saved data from localStorage
     $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
     ///need to repeat line 21 for all the other hours
 
 
     // display current day on page
+    $('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+    // display current time on page
+    $('#currentTime').text(moment().format("h:mm:ss a"))
 });
